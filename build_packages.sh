c6de31248c7953d2c412e1ad5be06fbeeb9b246a
emubastard <james@0mhz.net>
2024-03-13 21:14:08 -0600

    Rename configuration to align with MiSTer

--- build_packages.sh
+++ build_packages.sh
@@ -3,6 +3,7 @@
 MGL_DIR=$(realpath ./mgls)  # Default MGL directory relative to the script's location
 MEDIA_BASE=$HOME
 TARGET_DIR=$(realpath ./target)
+CONFIG_FILE=$(realpath config/AO486.CFG"")
 
 echo "Using MEDIA_DIR: $MEDIA_DIR"
 echo "Using TARGET_DIR: $TARGET_DIR"
@@ -13,21 +14,42 @@ echo "Preparing the target directory..."
 if [ -d "$TARGET_DIR" ]; then
     rm -rf "$TARGET_DIR"
 fi
+
+rm -fr $TARGET_DIR
 mkdir -p "$TARGET_DIR"
+mkdir -p "$TARGET_DIR/zips"
 
 find "$MGL_DIR" -type f -name "*.mgl" | while read file; do
-  echo "File: $file"
+  echo "MGL File: $file"
+
+  # get the game contents directory name
   modified_xml=$(sed 's/&/\&amp;/g' "$file")
-  file_path=$(echo "$modified_xml" | xmllint --xpath 'string(//file[1]/@path)' -)
-  parent_dir_path=$(dirname "$file_path")
+  temp_path=$(echo "$modified_xml" | xmllint --xpath 'string(//file[1]/@path)' -)
+  parent_dir_path=$(dirname "$temp_path")
+
+  # create a source directory with the media base and game content name
   source_dir="$MEDIA_BASE/$parent_dir_path"
+  # create a path where to copy the files needed for a zip distribution of the game
   new_target=$TARGET_DIR/tmp/games/ao486/$parent_dir_path
-  
-  echo "File Path $file_path"
-  echo "Parent dir path path: $parent_dir_path"
-  echo "Source directory: $source_dir"
-  echo "New targt: $new_target"
+
+  # create zip file name for distribution
+  zip_file=$(basename "$file" | cut -d. -f1).zip
+
+  echo "Source Directory: $source_dir"
+  echo "Target Directory: $new_target"
+  echo "Zip archive: $zip_file"
+
+  rm -fr $TARGET_DIR/tmp
+  mkdir -p "$new_target"
+  cp -r "$source_dir"/* "$new_target"
+
+  mkdir -p "$TARGET_DIR/tmp/config"
+  cp $CONFIG_FILE "$TARGET_DIR/tmp/config"
+
+  cd "$TARGET_DIR/tmp"
+  zip -r "$TARGET_DIR/zips/$zip_file" *
+  cd "$TARGET_DIR"
   echo "---------------------------------------------------------------------------"
-  #mkdir -p $new_target
-  #cp $MEDIA_DIR/$parent_dir/* $new_target
+
+
 done
